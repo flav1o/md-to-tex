56529ac39369a9fe827770908138a4eb93cf645b
Flávio Costa <61461590+flav1o@users.noreply.github.com>
2022-12-31 22:44:07 +0000

    feat(types): add types file (#3)
    * feat(express): add express basic setup
    
    * feat(middlewares): add not found middleware
    
    * chore(env): add env variables
    
    * chore(vars): remove unused vars
    
    * refactor(generateEmail): add response to generate email endpoint object
    
    * refactor(types): add generic response object

--- src/controller/ai.controller.ts
+++ src/controller/ai.controller.ts
@@ -2,8 +2,11 @@ import { Request, Response } from "express";
 import generateText from "../service/ai.service";
 
 const generateEmail = async (req: Request, res: Response) => {
-	const aiGeneratedText = await generateText("talk about dogs");
-	return res.status(200).send({ response: aiGeneratedText });
+	const { prompt } = req.body;
+	if (!prompt) return res.status(400).send({ error: "No prompt provided" });
+
+	const response = await generateText(prompt);
+	return res.send(response);
 };
 
 export { generateEmail };
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface ApiResponse<T> {
+	body: T;
+	statusCode: number;
+}
