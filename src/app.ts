b9e6c277b4324b0d826a5c1930657ce8f3854da2
flav1o <flaviodanielmartinscosta@gmail.com>
2022-12-31 19:27:54 +0000

    chore(vars): remove unused vars

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import ENV_VARIABLES from "./config/env";
 import AiRouter from "./router/ai.router";
 import notFound from "./middleware/fourofour.middleware";
 import serverListener from "./server";
-
-const SERVER_PORT = ENV_VARIABLES.SERVER_PORT;
 
 const app = express();
 app.use(express.json());
